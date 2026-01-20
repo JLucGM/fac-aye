3ec4293f9d0657761feda827a0032ab58690dc9c
Jean Gouirand <elluc09@gmail.com>
2026-01-20 17:03:17 -0400

    actualizacion de assets

---
+++ public/build/assets/heading-Dl2IDkKw.js
@@ -0,0 +1,6 @@
+import{j as e,$ as c}from"./app-DLKrPyog.js";import{B as r}from"./button-BJaQVxI9.js";import{c as n}from"./createLucideIcon-CSMuHRtA.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M6 8L2 12L6 16",key:"kyvwex"}],["path",{d:"M2 12H22",key:"1m8cig"}]],l=n("MoveLeft",o);function f({title:a,description:s,href:t,children:i}){return e.jsxs("div",{className:"mb-6 space-y-0.5 flex justify-between items-center",children:[e.jsxs("div",{className:"flex justify-start",children:[t&&e.jsx(r,{variant:"link",className:"px-0 ",asChild:!0,children:e.jsx(c,{href:route(t),children:e.jsx(l,{className:""})})}),e.jsxs("div",{className:"",children:[e.jsx("h2",{className:"text-xl font-semibold tracking-tight",children:a}),s&&e.jsx("p",{className:"text-muted-foreground text-sm",children:s})]})]}),i]})}export{f as H};
